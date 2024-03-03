60bfb8e4db0771df56bf611e94a688ba4729273a
JAdithyaNaik <jatothadithyanaik@gmail.com>
2024-03-03 10:01:56 +0530

    adding up things

--- task2_calculator.py
+++ task2_calculator.py
@@ -12,7 +12,7 @@ result =0
 def main():
     print("\n\n\t\t\tby @JATOTH ADITHYA NAIK")
     print("________________________________________")
-    print("***CALCULATOR APP***")
+    print("\n\t\t\t***CALCULATOR APP***\n")
     while True :
         print("1.ADDITION")
         print("2.SUBTRACTION")
@@ -23,7 +23,9 @@ def main():
         print("5.POWER")
         a = int(input("\nEnter the first Operand :"))
         b = int(input("\nEnter the Second Operand :"))
+        choice = print("Enter your Choice:")
 
+        if():
 # main(recursion)--used to repeated n no.of times
 if __name__ == "__main__":
     main()
