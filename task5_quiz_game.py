ff5b2474192b92e736dc00ac73f90a0d4b38707f
adithya-naik <jatothadithyanaik@gmail.com>
2024-03-03 18:22:14 +0530

    Started Task_5 !!

---
+++ task5_quiz_game.py
@@ -0,0 +1,17 @@
+#COMMAND LINE APPLICATION OF A QUIZ GAME
+
+#Author : Jatoth Adithya Naik
+#for    : Intenship (TASK-5)
+
+# Discription :
+# this command appplication simulation of a quiz game
+# user has to select the correct answer for the question displayed out there
+
+score = 0
+print("\n\n\t\t\tby @JATOTH ADITHYA NAIK")
+print("\n\t\t\t *** QUIZ GAME ***\n")
+print('''\tRULES :\n
+    1. For Every Correct Answer Score Increases by +5.
+    2. For Every Wrong Answer Score Decreases By -3.
+    3. For Answersing "PASS" score decreases by -1''')
+
