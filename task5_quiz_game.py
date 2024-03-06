e6ca266bdd81caa1a19e863216e7fa0d60737ac3
Jatoth Adithya Naik <jatothadithyanaik@gmail.com>
2024-03-06 17:59:56 +0530

    commented some things

--- task5_quiz_game.py
+++ task5_quiz_game.py
@@ -6,6 +6,12 @@
 # Discription :
 # this command appplication simulation of a quiz game
 # user has to select the correct answer for the question displayed out there
+# RULES :\n
+#     1. For Every Correct Answer Score Increases by +5.
+#     2. For Every Wrong Answer Score Decreases By -3.
+#     3. For not answering score decreases by -1.
+#     4. Consists of 5 Questions.
+#     5. Min.Marks to pass 15
 
 score = 0
 print("\n\n\t\t\tby @JATOTH ADITHYA NAIK")
