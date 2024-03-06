4f5139958e54605c2e866a29dc264446a4973ca0
adithya-naik <jatothadithyanaik@gmail.com>
2024-03-06 16:57:26 +0530

    ADDED 3 QUESTIONS

--- task5_quiz_game.py
+++ task5_quiz_game.py
@@ -13,5 +13,99 @@ print("\n\t\t\t *** QUIZ GAME ***\n")
 print('''\tRULES :\n
     1. For Every Correct Answer Score Increases by +5.
     2. For Every Wrong Answer Score Decreases By -3.
-    3. For Answersing "PASS" score decreases by -1''')
+    3. For not answering score decreases by -1.
+    4. Consists of 5 Questions.
+    5. Min.Marks to pass 15
+    ''')
 
+print(''' 
+I/V) What type of software enables you to communicate with the computer and acts as a coordinator between the hardware and all systems that are runnning ? 
+ 
+     1 . Web application
+     2 . Operating system
+     3 . Word processor
+     4 . Web browser
+     ''')
+
+a1 = input("Select the Correct answer (if you want to don't answer , enter SPACE bar): ")
+
+if(a1=="2"):
+    score = score+5
+    print('''
+    Congratulations...!!!   Score = +5
+    ''')
+elif(a1==" "):
+    score = score-1
+    print('''
+    You have not answered...!!!  Score = -1
+    ''')
+else:
+    score= score-3
+    print('''
+    Wrong answer..!!!  Score = -3
+    ''')
+
+
+print(''' 
+II/V) What type of storage does a computer use when a user enters data into an online form and before they submit the form ?
+ 
+     1 . Long-term storage
+     2 . Cookies
+     3 . Short-term storage
+     4 . Processing storage
+     ''')
+
+a1 = input("Select the Correct answer (if you want to don't answer , enter SPACE bar): ")
+
+if(a1=="3"):
+    score = score+5
+    print('''
+    Congratulations...!!!   Score = +5
+    ''')
+elif(a1==" "):
+    score = score-1
+    print('''
+    You have not answered...!!!  Score = -1
+    ''')
+else:
+    score= score-3
+    print('''
+    Wrong answer..!!!  Score = -3
+    ''')
+
+
+print(''' 
+III/V) A computer must translate the data it recieves from the input devices into 0's and 1's so that it can either store or process the data. What is this system of 0's and 1's called ? 
+ 
+     1 . Binary Code
+     2 . Scripting System
+     3 . Long-term Storage system
+     4 . Input and output system
+     ''')
+
+a1 = input("Select the Correct answer (if you want to don't answer , enter SPACE bar): ")
+
+if(a1=="1"):
+    score = score+5
+    print('''
+    Correct answer...!!!   Score = +5
+    ''')
+elif(a1==" "):
+    score = score-1
+    print('''
+    You have not answered...!!!  Score = -1
+    ''')
+else:
+    score= score-3
+    print('''
+    Wrong answer..!!!  Score = -3
+    ''')
+
+
+    # prints your final answer
+print("\n\t\tYOUR SCORE = ",score)
+
+if(score == 15 ):
+    print("\t\t\n CONGRATULATIONS......YOU HAVE PASSED THE TEST\n")
+else :
+    print("\t\t\n BETTER LUCK NEXT TIME......YOU HAVE FAILED THE TEST\n")
